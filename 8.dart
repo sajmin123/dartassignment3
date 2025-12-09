39aa3b7789bd2799c745640d35f1da7a578ee4aa
sajmin(0182320012101208) <sajminakther172@gmail.com>
2025-12-09 07:26:36 -0800

    Add files via upload

---
+++ 8.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 10;
+  int b = 20;
+
+  int result = add(a, b);
+
+  print("The sum of $a and $b is $result");
+}
+
+int add(int num1, int num2) {
+  return num1 + num2;
+}
